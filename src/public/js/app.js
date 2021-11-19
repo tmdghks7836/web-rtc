01ab1aba44ef0a111b61e19f3d60a029cea37f27
DESKTOP-VSDKPQV\tmdgh <tmdghks7836@naver.com>
2021-11-19 17:35:58 +0900

    방 리스트 조회

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,9 +2,46 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-function serverDone(msg) {
-    console.log("the back-end says : ", msg)
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message){
+    const ul = chat.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const inputValue = input.value;
+    socket.emit("new_message", inputValue, roomName, () =>{
+        addMessage(`You: ${inputValue}`);
+    });
+    input.value = ""
+}
+
+function handleNickNameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+
+}
+
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+
+    nameForm.addEventListener("submit", handleNickNameSubmit)
+    msgForm.addEventListener("submit", handleMessageSubmit)
 }
 
 
@@ -12,9 +49,35 @@ function handleRoomSubmit(event) {
     event.preventDefault();
 
     const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, serverDone);
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 
 }
 
 form.addEventListener("submit", handleRoomSubmit)
+
+socket.on("welcome", (user) => {
+
+    addMessage(`${user} joined`); 
+})
+
+socket.on("bye", (user) => {
+    addMessage(`${user} left ㅜㅜ`)
+})
+
+socket.on("new_message", (msg) => {
+    addMessage(msg)
+})
+
+socket.on("room_change", (rooms) => {
+
+const roomList = welcome.querySelector("ul");
+roomList.innerHTML = "";
+ rooms.forEach(room => {
+     const li = document.createElement("li");
+     li.innerText = room;
+     roomList.append(li);
+ });
+ 
+});
--- src/server.js
+++ src/server.js
@@ -16,14 +16,54 @@ const handleListen = () => console.log("Listening on http://localhost:3000")
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms(){
+    const {
+        sockets: {
+            adapter: {
+                sids, rooms
+            },
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_,key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
 wsServer.on("connection", socket => {
-    console.log(socket);
-    socket.on("enter_room", (roomName, done) => {
-         console.log(roomName);
-         setTimeout(() => {
-             done("hello front end?");
-         })
+
+    socket["nickname"] = "Anon"
+    socket.onAny((event)=> {
+        // console.log(wsServer.sockets.adapter)
+        console.log(`Socket Event: ${event}`);
     })
+    // console.log(socket);
+    socket.on("enter_room", (roomName, done) => {
+         socket.join(roomName);
+         done();
+         socket.to(roomName).emit("welcome",socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
+    })
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => {
+            socket.to(room).emit("bye", socket.nickname);
+        });
+    })
+
+    socket.on("disconnect", () =>{
+        wsServer.sockets.emit("room_change", publicRooms());
+    })
+
+    socket.on("new_message", (msg, roomName, done) =>{
+
+        socket.to(roomName).emit("new_message", `${socket.nickname}: ${msg}`); 
+        done();       
+    })
+
+    socket.on("nickname", nickname => socket["nickname"] = nickname)
 })
 // const wss = new WebSocket.Server({ server });
 
