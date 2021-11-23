47f98e98c2fd3c14666397923446637ad67410a1
tmdghks7836 <tmdghks7836@naver.com>
2021-11-23 18:20:45 +0900

    화상 통화 완료  구글에서 제공하는 npm i localtunnel, lt --port 3000 을 통해 다른네트워크 연결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,7 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const peersFace = document.getElementById("peersFace");
 const muteButton = document.getElementById("mute");
 const cameraButton = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
@@ -15,6 +16,7 @@ let muted = true;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getMedia(deviceId) {
 
@@ -98,27 +100,39 @@ function handleCameraClick() {
     }
 }
 
-function handleCameraChange() {
-    getMedia(camerasSelect.value)
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+
+        const videoSender = myPeerConnection.getSenders().find(sender =>
+            sender.track.kind === "video");
+        console.log(videoSender);
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 muteButton.addEventListener("click", handleMuteClick);
 cameraButton.addEventListener("click", handleCameraClick);
-camerasSelect.addEventListener("input", handleCameraChange);
+camerasSelect.addEventListener("click", handleCameraChange);
 
 //welcome form
 
 const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
-function handleWelcomeSubmit() {
+//승환이 방생성 or 진입
+async function handleWelcomeSubmit(event) {
     event.preventDefault();
     const input = welcomeForm.querySelector("input");
-    socket.emit("join_room", input.value, startMedia);
+
+    await startMedia();
+    socket.emit("join_room", input.value); //방 진입
     roomName = input.value;
     input.value = ""
 }
 
-async function startMedia(){
+
+async function startMedia() {
     welcome.hidden = true;
     call.hidden = false;
     await getMedia();
@@ -127,9 +141,13 @@ async function startMedia(){
 
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
-// socket Code
+//------------------------------------------------------- socket Code
 
+//새로들어온사람이 들어왔다고 기존사람들이 받고 offer 정보 새로들어온사람에게 넘겨줌 
 socket.on("welcome", async () => {
+    myDataChannel = myPeerConnection.createDataChannel("chat");
+    myDataChannel.addEventListener("message", console.log)
+    console.log("made data channel")
     console.log("someone come in")
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
@@ -137,17 +155,62 @@ socket.on("welcome", async () => {
     socket.emit("offer", offer, roomName);
 })
 
-socket.on("offer", offer => {
-    console.log(offer);
+//새로들어온사람이 offer 정보를 기존사람들에게 받고 그 사람들에게 answer 전송
+socket.on("offer", async (offer) => {
+    myPeerConnection.addEventListener("datachannel", (event) =>{
+        myDataChannel = event.channel;
+        myDataChannel.addEventListener("message", console.log)
+    })
+    console.log("received the offer")
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName)
 })
 
+//기존사람들이 새로들어온사람에게 연결정보 응답을 받고
+socket.on("answer", async (answer) => {
+    console.log("received the answer")
+    myPeerConnection.setRemoteDescription(answer);
+})
 
-// RTC code
+socket.on("ice", ice => {
+    console.log("recieved ice candidate")
+    myPeerConnection.addIceCandidate(ice);
+})
 
-function makeConnection(){
-    myPeerConnection = new RTCPeerConnection();
+//-------------------------------------------- RTC code
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [{
+            urls: [
+                "stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302",
+            ]
+        }]
+    });
+    myPeerConnection.addEventListener("icecandidate", handleIce)
+    myPeerConnection.addEventListener("addstream", handleAddStream)
     console.log(myStream.getTracks())
     myStream.getTracks().forEach(track => {
         myPeerConnection.addTrack(track, myStream)
     })
+}
+
+// 내가 가지고있는 stream 장치 설정 정보를 peer to peer 연결 시 받음 
+function handleIce(data) {
+    socket.emit("ice", data.candidate, roomName)
+    console.log("got ice candidate")
+    console.log(data);
+}
+
+function handleAddStream(data) {
+    console.log("get an event from my peer");
+    console.log("상대 stream ", data.stream);
+    console.log("my stream ", myStream);
+    peersFace.srcObject = data.stream;
 }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import express from "express";
-import {Server} from "socket.io"
+import {
+    Server
+} from "socket.io"
 import http from "http";
-import { instrument } from "@socket.io/admin-ui"
+import {
+    instrument
+} from "@socket.io/admin-ui"
 const app = express();
 
 app.set("view engine", "pug");
@@ -17,22 +21,24 @@ const handleListen = () => console.log("Listening on http://localhost:3000")
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
-wsServer.on("connection", socket =>{
-    socket.on("join_room", (roomName,done) => {
+wsServer.on("connection", socket => {
+    socket.on("join_room", (roomName, done) => {
         socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome")
+        socket.to(roomName).emit("welcome") // 다른 방 사람들에게 방에 들어왔다고 알림
     });
-    socket.on("offer",(offer, roomName)=> {
-        socket.to(roomName).emit("offer", offer);
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer); //다른사람들에게 connection 정보 알려줌
     })
-} )
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer); //다른사람들에게 연결 응답을 보냄
+    })
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice); //다른사람들에게 연결 응답을 보냄
+    })
+})
 
 instrument(wsServer, {
     auth: false,
 });
 
 httpServer.listen(3000, handleListen);
-
-
-
